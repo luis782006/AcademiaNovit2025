0b62d0e99b99a315e219963294e9a0082531e539
luis782006 <luisfelipe782006@gmail.com>
2025-07-07 13:20:15 -0300

    Correcion de gitignore

---
+++ AcademiaNovit/Program.cs
@@ -0,0 +1,66 @@
+using AcademiaNovit;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using Serilog;  
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region configuracion del Serilog
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
+
+builder.Configuration.AddEnvironmentVariables();
+
+# endregion
+
+#region leer variables de entorno
+
+builder.Configuration.AddEnvironmentVariables();
+
+#endregion
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+#region configurar conexion Postgress
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+#endregion
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapScalarApiReference();
+
+app.MapControllers();
+
+#region keep alive endpoint
+
+app.MapGet("/keep-alive", () => new
+{
+    status = "alive",
+    timestamp = DateTime.UtcNow
+});
+
+#endregion
+
+app.Run();
+
+public partial class Program { } // This partial class is required for the WebApplicationFactory to work properly in tests. 
